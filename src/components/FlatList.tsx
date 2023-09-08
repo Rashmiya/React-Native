21889b47feb3a8db4442b6ad42ad59b717f13ae3
Rashmiya <rashmiyarafeek1200@gmail.com>
2023-09-08 22:44:26 +0530

    react-native flexBox

--- src/components/FlatList.tsx
+++ src/components/FlatList.tsx
@@ -1,12 +1,54 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
 const FlatList = () => {
   return (
     <View>
       <Text>FlatList</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Card 1</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Card 2</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Card 3</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: 8,
+    marginHorizontal: 4,
+  },
+  cardOne: {
+    backgroundColor: 'red',
+    border: '1px solid red',
+  },
+  cardTwo: {
+    backgroundColor: 'green',
+    border: '1px solid green',
+  },
+  cardThree: {
+    backgroundColor: 'blue',
+    border: '1px solid blue',
+  },
+});
 
 export default FlatList;
